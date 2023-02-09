4e75ce385b7125d60478d8915d54533a609b882a
oshinokobot <chainsawmantwitterbot@gmail.com>
2023-02-09 16:16:50 +0000

    auth

--- auth.py
+++ auth.py
@@ -29,6 +29,22 @@ def getTwitterAuth():
 def getGoogleAuth():
 
     gauth = GoogleAuth()
+    # Try to load saved client credentials
+    gauth.LoadCredentialsFile("GoogleDriveCredentials.txt")
+    if gauth.credentials is None:
+        # Authenticate if they're not there
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        # Refresh them if expired
+        print("Google Drive Token Expired, Refreshing")
+        gauth.Refresh()
+    else:
+        # Initialize the saved creds
+        gauth.Authorize()
+    # Save the current credentials to a file
+    gauth.SaveCredentialsFile("GoogleDriveCredentials.txt")
     drive_auth = GoogleDrive(gauth)
 
     return drive_auth
+
+
